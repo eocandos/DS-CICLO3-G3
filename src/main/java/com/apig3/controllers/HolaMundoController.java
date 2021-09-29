3df448856d61f1c09072093b21c63bc88d658809
Erick Ocando <erick.ocando.mt@correo.usa.edu.co>
2021-09-29 07:05:50 -0500

    Creacion controlador y metodo GET

---
+++ src/main/java/com/apig3/controllers/HolaMundoController.java
@@ -0,0 +1,17 @@
+package com.apig3.controllers;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/saludar")
+public class HolaMundoController {
+
+    @GetMapping("/hola")
+    public String saludar() {
+        return "Hola Grupo 3 DS";
+    }
+
+}
